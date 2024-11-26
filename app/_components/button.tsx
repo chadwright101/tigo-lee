4cafd7990949ffe6690a8b9af5b15980029c4d49
Chad Wright <justdeejays@gmail.com>
2024-11-26 11:33:37 +0200

    init commit

---
+++ app/_components/button.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import classNames from "classnames";
+import Link from "next/link";
+import { useFormStatus } from "react-dom";
+
+interface Props {
+  backgroundColor?: "khaki" | "grey";
+  strokeWhite?: boolean;
+  onClick?: () => void;
+  typeSubmit?: boolean;
+  children?: React.ReactNode;
+  cssClasses?: string;
+  url?: string;
+  target?: string;
+  normalButton?: boolean;
+  disabled?: boolean;
+}
+
+const buttonStyles = ({
+  cssClasses,
+  backgroundColor,
+  strokeWhite,
+  disabled,
+}: Props) =>
+  classNames(
+    `flex gap-3 w-full items-center text-[20px] font-bold tracking-[0.025rem] text-white justify-center uppercase ease-in-out duration-300 py-1 px-7 ${cssClasses}`,
+    {
+      "bg-khaki border-4 border-khaki desktop:hover:bg-transparent desktop:hover:text-khaki":
+        backgroundColor === "khaki" && !strokeWhite,
+      "bg-grey border-4 border-white desktop:hover:bg-white desktop:hover:text-grey":
+        backgroundColor === "grey" && !strokeWhite,
+      "bg-khaki border-4 border-white desktop:hover:bg-white desktop:hover:text-grey":
+        backgroundColor === "khaki" && strokeWhite && !disabled,
+      "bg-khaki border-4 border-white":
+        backgroundColor === "khaki" && strokeWhite && disabled,
+    }
+  );
+
+const Button = ({
+  backgroundColor = "khaki",
+  onClick,
+  typeSubmit,
+  children,
+  cssClasses,
+  url = "#",
+  target,
+  normalButton,
+  disabled,
+  strokeWhite,
+}: Props) => {
+  const { pending } = useFormStatus();
+
+  if (!normalButton) {
+    return (
+      <Link
+        href={url}
+        target={target}
+        className={buttonStyles({
+          cssClasses,
+          backgroundColor,
+          strokeWhite,
+          disabled,
+        })}
+        aria-label={children as string}
+      >
+        {children}
+      </Link>
+    );
+  } else {
+    return (
+      <button
+        className={buttonStyles({
+          cssClasses,
+          backgroundColor,
+          strokeWhite,
+          disabled,
+        })}
+        type={typeSubmit ? "submit" : "button"}
+        onClick={onClick}
+        disabled={disabled}
+        aria-label={children as string}
+      >
+        {pending ? <div className="spinner"></div> : <>{children}</>}
+      </button>
+    );
+  }
+};
+
+export default Button;
---
+++ app/_lib/social-icons.tsx
@@ -0,0 +1,121 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import whatsapp from "@/public/assets/icons/whatsapp.svg";
+import facebook from "@/public/assets/icons/facebook.svg";
+import instagram from "@/public/assets/icons/instagram.svg";
+import whatsappGrey from "@/public/assets/icons/whatsapp-grey.svg";
+import facebookGrey from "@/public/assets/icons/facebook-grey.svg";
+import instagramGrey from "@/public/assets/icons/instagram-grey.svg";
+
+interface Props {
+  cssClasses?: string;
+  small?: boolean;
+  grey?: boolean;
+}
+
+const SocialIcons = ({ cssClasses, small, grey }: Props) => {
+  if (small) {
+    return (
+      <ul className={`flex gap-2 ${cssClasses}`}>
+        <li>
+          <Link
+            href=""
+            target="_blank"
+            className="tablet:tablet:hover:opacity-80 ease-in-out duration-200"
+            aria-label="View our Facebook page"
+          >
+            <Image
+              src={!grey ? facebook : facebookGrey}
+              alt="View our Facebook page"
+              width={20}
+              height={20}
+            />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href=""
+            target="_blank"
+            className="tablet:tablet:hover:opacity-80 ease-in-out duration-200"
+            aria-label="View our Instagram profile"
+          >
+            <Image
+              src={!grey ? instagram : instagramGrey}
+              alt="View our Instagram profile"
+              width={20}
+              height={20}
+            />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href=""
+            target="_blank"
+            className="tablet:tablet:hover:opacity-80 ease-in-out duration-200"
+            aria-label="Get in touch on WhatsApp"
+          >
+            <Image
+              src={!grey ? whatsapp : whatsappGrey}
+              alt="Get in touch on WhatsApp"
+              width={20}
+              height={20}
+            />
+          </Link>
+        </li>
+      </ul>
+    );
+  } else {
+    return (
+      <ul className={`flex gap-5 ml-0.5 ${cssClasses}`}>
+        <li>
+          <Link
+            href=""
+            className="grid h-10 w-10 -mx-2 -my-1.5 place-items-center tablet:hover:opacity-80 ease-in-out duration-200"
+            target="_blank"
+            aria-label="View our Facebook page"
+          >
+            <Image
+              src={!grey ? facebook : facebookGrey}
+              alt="View our Facebook page"
+              width={28}
+              height={28}
+            />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href=""
+            className="grid h-10 w-10 -mx-2.5 -my-1.5 place-items-center tablet:hover:opacity-80 ease-in-out duration-200"
+            target="_blank"
+            aria-label="View our Instagram profile"
+          >
+            <Image
+              src={!grey ? instagram : instagramGrey}
+              alt="View our Instagram profile"
+              width={28}
+              height={28}
+            />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="https://wa.me/27610093643?src=qr"
+            className="grid h-10 w-10 -mx-2.5 -my-1.5 place-items-center tablet:hover:opacity-80 ease-in-out duration-200"
+            target="_blank"
+            aria-label="Get in touch on WhatsApp"
+          >
+            <Image
+              src={!grey ? whatsapp : whatsappGrey}
+              alt="Get in touch on WhatsApp"
+              width={28}
+              height={28}
+            />
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+};
+
+export default SocialIcons;
